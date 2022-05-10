131fad0a382d60642f27bc4bb9eb533003c68fbe
William Justin <williamjustinxu@gmail.com>
2022-05-10 17:04:09 +0700

    update correspond to model.py

--- train_stage_1.py
+++ train_stage_1.py
@@ -7,11 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image
 from torchvision import transforms
 
-from model import ComponentEmbedding_LeftEye
-from model import ComponentEmbedding_RightEye
-from model import ComponentEmbedding_Nose
-from model import ComponentEmbedding_Mouth
-from model import ComponentEmbedding_Background
+from model import DeepFaceDrawing
 
 def get_args_parser():
     import argparse
@@ -19,7 +15,7 @@ def get_args_parser():
     parser.add_argument('--dataset', type=str, required=True, help='Path to training dataset.')
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--epochs', type=int, required=True)
-    parser.add_argument('--resume', type=str, default=None, help='Path to load weights.')
+    parser.add_argument('--resume', type=str, default=None, help='Path to load model weights.')
     parser.add_argument('--output', type=str, required=True, help='Path to save weights.')
     args = parser.parse_args()
     return args
@@ -50,51 +46,58 @@ def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Device : {device}')
     
-    CE_LeftEye = ComponentEmbedding_LeftEye()
-    CE_RightEye = ComponentEmbedding_RightEye()
-    CE_Nose = ComponentEmbedding_Nose()
-    CE_Mouth = ComponentEmbedding_Mouth()
-    CE_Background = ComponentEmbedding_Background()
-    
-    CE = [CE_LeftEye, CE_RightEye, CE_Nose, CE_Mouth, CE_Background]
+    model = DeepFaceDrawing(
+        CE=True, CE_encoder=True, CE_decoder=True,
+        FM=False, IS=False, manifold=False
+    )
     
     if args.resume:
-        for CEs in CE: CEs.load(args.resume)
-            
-    for CEs in CE: CEs.to(device)
-    
-    transform = CE_Background.transform
+        model.load(args.resume)
+        
+    model.to(device)
+        
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize(512),
+        transforms.ToTensor()
+    ])
     augmentation_transform = None
     dataset = Stage1Dataset(args.dataset, transform, augmentation_transform)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=10)
     
-    optimizers = [torch.optim.Adam(CEs.parameters(), lr=0.0002, betas=(0.5, 0.999)) for CEs in CE]
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0002, betas=(0.5, 0.999))
     criterion = torch.nn.MSELoss()
         
     for epoch in range(args.epochs):
-        for CEs in CE: CEs.train()
         
-        running_loss = [0 for _ in range(len(CE))]
+        running_loss = {
+            'left_eye' : 0,
+            'right_eye' : 0,
+            'nose' : 0,
+            'mouth' : 0,
+            'background' : 0
+        }
         
-        for sketch in tqdm(dataloader, desc=f'Epoch - {epoch+1} / {args.epochs}'):
-            for i in range(len(CE)):
-                cropped_sketch = CE[i].crop(sketch).to(device)
-                optimizers[i].zero_grad()
-                
-                prediction = CE[i](cropped_sketch)
-                loss = criterion(prediction, cropped_sketch)
+        model.train()
+        
+        for sketches in tqdm(dataloader, desc=f'Epoch - {epoch+1} / {args.epochs}'):
+            optimizer.zero_grad()
+            for key in model.keys:
+                X = model.CE[key].crop(sketches).to(device)
+                y = model.CE[key](X)
+                loss = criterion(y, X)
                 loss.backward()
-                optimizers[i].step()
-                
-                running_loss[i] += loss.item() * len(sketch) * len(dataloader.sampler)
-        
+                running_loss[key] += loss.item() * len(sketches) / len(dataloader.dataset)
+            optimizer.step()
+            
         print()    
         print(f'Epoch - {epoch+1} / {args.epochs}')
-        print(f'Loss    : {running_loss:.6f}')
+        for key, loss in running_loss.items():
+            print(f'Loss {key:10} : {loss:.6f}')
         print()
         
-        for CEs in CE: CEs.save(args.output)
-    
+        model.save(args.output)
+        
 if __name__ == '__main__':
     args = get_args_parser()
     print(args)
