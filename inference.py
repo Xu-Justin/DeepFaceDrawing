e78cfbaa7ce444ae66e172a093e82eef1a44eb17
intern-william@nodeflux-67 <justin@nodeflux.io>
2022-06-14 07:08:50 +0000

    support folder inference

--- inference.py
+++ inference.py
@@ -1,17 +1,29 @@
 import torch
+import os
 import datasets, models, utils
 
 def get_args_parser():
     import argparse
     parser = argparse.ArgumentParser(description='Deep Face Drawing: Inference')
     parser.add_argument('--weight', type=str, required=True, help='Path to load model weights.')
-    parser.add_argument('--image', type=str, required=True, help='Path to read image and be inferenced.')
+    parser.add_argument('--image', type=str, default=None, help='Path to read image and be inferenced.')
+    parser.add_argument('--folder', type=str, default=None, help='Path to folder to be inference.')
     parser.add_argument('--output', type=str, required=True, help='Path to save result image.')
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--manifold', action='store_true', help='Use manifold projection in the model.')
     args = parser.parse_args()
     return args
-    
+
+def inference(model, path_image, path_output, device, args):
+    image = datasets.dataloader.load_one_sketch(path_image, simplify=True, device=args.device).unsqueeze(0).to(device)
+    print(f'Loaded image from {path_image}')
+
+    with torch.no_grad():
+        result = model(image)
+    result = utils.convert.tensor2PIL(result[0])
+    result.save(path_output)
+    print(f'Saved result to {path_output}')
+
 def main(args):
     
     device = torch.device(args.device)
@@ -27,14 +39,15 @@ def main(args):
     model.to(device)
     model.eval()
     
-    image = datasets.dataloader.load_one_sketch(args.image, simplify=True, device=args.device).unsqueeze(0).to(device)
-    print(f'Loaded image from {args.image}')
-
-    with torch.no_grad():
-        result = model(image)
-    result = utils.convert.tensor2PIL(result[0])
-    result.save(args.output)
-    print(f'Saved result to {args.output}')
+    if args.image:
+        inference(model, args.image, args.output, device, args)
+    
+    if args.folder:
+        os.makedirs(args.output, exist_ok=True)
+        for file_name in os.listdir(args.folder):
+            path_image = os.path.join(args.folder, file_name)
+            path_output = os.path.join(args.output, file_name)
+            inference(model, path_image, path_output, device, args)
     
 if __name__ == '__main__':
     args = get_args_parser()
